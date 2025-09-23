b957cc43e2a79a96d774070b5da7834f01e35b96
Javier Ponce <imr3ivaj@gmail.com>
2025-09-23 17:55:50 -0600

    feat: implement prompt input component with attachment support and suggestions feature

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,56 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { Button } from '@/components/ui/button'
+import {
+  PromptInput,
+  PromptInputBody,
+  PromptInputSubmit,
+  PromptInputTextarea,
+  PromptInputToolbar,
+  PromptInputTools,
+} from '@/components/ai-elements/prompt-input';
+import { Suggestion, Suggestions } from '@/components/ai-elements/suggestion';
+
+
+import { useState } from 'react';
 
 export const Route = createFileRoute('/')({
   component: Home,
 })
 
 function Home() {
+
+  const [message, setMessage] = useState('');
+
   return (
-    <div>
-      <Button>Click me</Button>
+    <div className="flex justify-center">
+      <div className="w-full max-w-[48rem]">
+        <div className="mt-60 flex justify-center mb-6">
+          <span className="text-4xl">🧁</span>
+        </div>
+        <PromptInput onSubmit={(message, event) => {
+          console.log(message, event);
+        }} className="relative">
+          <PromptInputBody>
+            <PromptInputTextarea
+              onChange={(e) => { setMessage(e.target.value) }}
+              value={message}
+              placeholder="Describe tu formulario"
+            />
+          </PromptInputBody>
+          <PromptInputToolbar>
+            <PromptInputTools>
+            </PromptInputTools>
+            <PromptInputSubmit
+              disabled={false}
+              status={'ready'}
+            />
+          </PromptInputToolbar>
+        </PromptInput>
+
+        <Suggestions className="mt-4">
+          <Suggestion suggestion="Onboarding Persona Física" />
+          <Suggestion suggestion="Onboarding Persona Moral" />
+        </Suggestions>
+      </div>
     </div>
   )
 }
