e15bd05d290af4a91afc509dfe48ec07eca685e4
Javier Ponce <imr3ivaj@gmail.com>
2025-10-09 21:11:05 -0600

    chore: update .gitignore and package configuration
    - Added .netlify to .gitignore to prevent local Netlify folder from being tracked.
    - Included @netlify/vite-plugin-tanstack-start dependency in package.json for enhanced integration with Netlify.
    - Updated pnpm-lock.yaml to reflect the new dependency and its version.
    - Added sharp to the list of onlyBuiltDependencies in pnpm-workspace.yaml for better workspace management.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import viteReact from "@vitejs/plugin-react";
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 export default defineConfig({
   server: {
@@ -13,6 +14,7 @@ export default defineConfig({
       projects: ["./tsconfig.json"],
     }),
     tanstackStart(),
+    netlify(),
     viteReact(),
     tailwindcss(),
   ],
